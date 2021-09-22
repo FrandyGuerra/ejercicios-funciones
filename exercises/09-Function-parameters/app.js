c1e04a96ecffc72c615f89851e0e1533ad9c98fd
FrandyGuerra <frandyguerra2012@gmail.com>
2021-09-22 21:25:55 +0000

    Estos son los ejercicios resueltos de funciones

--- exercises/09-Function-parameters/app.js
+++ exercises/09-Function-parameters/app.js
@@ -1,5 +1,8 @@
 // Your code goes here:
+ function renderPerson(nombre,fechaNac,color,edad,genero){
 
+  return `${nombre} is a ${edad} years old ${genero} born in ${fechaNac} with ${color} eyes`;
+ }
 
 
   ///*** Do not edit below this line ****/
